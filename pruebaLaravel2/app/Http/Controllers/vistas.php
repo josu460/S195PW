ecc42518587982d8bb85b419f0b9b2dd851aff7f
Josue Rivera <122042206@upq.edu.mx>
2024-10-11 18:26:34 -0600

    controladores

---
+++ pruebaLaravel2/app/Http/Controllers/vistas.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class vistas extends Controller
+{
+
+    public function home()
+    {
+        return view('welcome');
+    }
+    public function formulario()
+    {
+        return view('formulario');
+    }
+
+    public function consulta()
+    {
+        return view('clientes');
+    }
+}
--- pruebaLaravel2/routes/web.php
+++ pruebaLaravel2/routes/web.php
@@ -2,14 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\vistas;
+
+
+// ruta para trabajar con un controlador 
+Route :: get('/', [vistas::class, 'home']) -> name ('rutainicio');
+Route :: get('/formulario', [vistas::class, 'formulario']) -> name ('rutaformulario');
+Route :: get('/clientes', [vistas::class, 'consulta']) -> name ('rutaclientes');
+
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::view("/","welcome")->name('rutainicio');
+// Route::view("/","welcome")->name('rutainicio');
 
-Route::view('/formulario', 'formulario')->name('rutaformulario');
+// Route::view('/formulario', 'formulario')->name('rutaformulario');
 
-Route::view('/clientes', 'clientes')-> name('rutaclientes');
+// Route::view('/clientes', 'clientes')-> name('rutaclientes');
 
 Route::view('/componentes', 'componentes') -> name('componentes');
