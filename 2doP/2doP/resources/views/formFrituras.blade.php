81bdbe8aaa72384dcf70f85bd670f463842e2342
Josue Rivera <122042206@upq.edu.mx>
2024-11-04 18:07:10 -0600

    avance 6:05

---
+++ 2doP/2doP/resources/views/formFrituras.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+</head>
+<body>
+
+    <h1 class="text-center text-success mt-5 mb-4">Registro Frituras</h1>
+
+    <div class="container col-md-5">
+
+    <form>
+
+        <div class="mb-3">
+            <label  class="form-label">Nombre: </label>
+            <input type="text" class="form-control" >
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Sabor(s): </label>
+            <input type="text" class="form-control" >
+        </div>                
+
+        <div class="mb-3">
+            <label  class="form-label">Peso: </label>
+            <input type="text" class="form-control"  >
+        </div>
+
+        <button type="" class="btn btn-primary "> Guardar Fritura</button>
+</div>
+
+</body>
+</html>
