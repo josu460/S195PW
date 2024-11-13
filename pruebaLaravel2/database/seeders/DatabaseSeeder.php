b9f01b4ac408de594b25d7f2773e90fb015e9337
Josue Rivera <122042206@upq.edu.mx>
2024-11-13 15:32:26 -0600

    seeders practica #7

--- pruebaLaravel2/database/seeders/DatabaseSeeder.php
+++ pruebaLaravel2/database/seeders/DatabaseSeeder.php
@@ -13,11 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        // esta linea es para mandar llamar tus seeder que creamos 
+        $this->call(ClienteTableSeeder::class);
+
+
+
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
     }
 }
